a50e6f672c0353659d7fe9d23124ce7719698b7e
Wesley Nascimento <wnascx@gmail.com>
2025-03-29 14:57:54 -0300

    feat: ensure createCalibratorController returns 400 if description not provided

--- src/presentation/controllers/mocks/create-calibrator/index.ts
+++ src/presentation/controllers/mocks/create-calibrator/index.ts
@@ -3,11 +3,13 @@ import { ReqWithoutStatus } from './without-status'
 import { ReqWithoutModel } from './without-model'
 import { ReqWithoutCertificate } from './without-certificate'
 import { ReqWithoutSerialNumber } from './without-serial-number'
+import { ReqWithoutDescription } from './without-description'
 
 export {
     ReqWithoutCode,
     ReqWithoutStatus,
     ReqWithoutModel,
     ReqWithoutCertificate,
-    ReqWithoutSerialNumber
+    ReqWithoutSerialNumber,
+    ReqWithoutDescription
 }
--- src/presentation/controllers/mocks/create-calibrator/without-description.ts
+++ src/presentation/controllers/mocks/create-calibrator/without-description.ts
@@ -1,11 +1,10 @@
-export const CompleteReq = {
+export const ReqWithoutDescription = {
     body: {
         code: "any_code", //ok
         status: "any_status", //ok
         model: "any_model", //ok
         certificate: "any_certificate", //ok
         serialNumber: "any_serial_number", //ok
-        description: "any_description", //ok
         calibrationCate: "any_calibration_date",
         calibrationFrequency: "any_calibration_frequency",
         nextCalibraton: "any_next_calibration",
