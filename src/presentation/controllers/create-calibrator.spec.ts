ac339f7a668b402fe43fbd8162e9ac544366eff6
Wesley Nascimento <wnascx@gmail.com>
2025-03-29 14:11:00 -0300

    test: ensure CreateCalibratorController returns 400 if code and status not provided

---
+++ src/presentation/controllers/create-calibrator.spec.ts
@@ -0,0 +1,19 @@
+import { CreateCalibratorController } from "./create-calibrator"
+import { ReqWithoutCode, ReqWithoutStatus } from '../controllers/mocks/create-calibrator'
+const createCalibratorController = new CreateCalibratorController()
+
+describe("CreateCalibratorController", () => {
+    test("Should return 400 if code is no provided", async () => {
+        const response = await createCalibratorController.handle(ReqWithoutCode)
+        expect(response.statusCode).toBe(400)
+        expect(response.body.error).toEqual(new Error('Missing param: code'))
+    })
+})
+
+describe("CreateCalibratorController", () => {
+    test("Should return 400 if status is no provided", async () => {
+        const response = await createCalibratorController.handle(ReqWithoutStatus)
+        expect(response.statusCode).toBe(400)
+        expect(response.body.error).toEqual(new Error('Missing param: status'))
+    })
+})
